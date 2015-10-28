af816990ef9488e595f457ad37a832568fbcbb9f
Ben Glassman <ben@vtdesignworks.com>
2015-10-28 00:44:04 -0400

    More sane css name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import jQuery from 'jquery';
-import wut from 'wut.css';
+import styles from 'styles.css';
 
 export default class App extends Component {
     constructor(props) {
